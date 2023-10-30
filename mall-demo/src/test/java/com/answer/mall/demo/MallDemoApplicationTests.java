9c7b0e49a355095bd881a68772fb624be5a43a31
AnswerYL <answer_wx@163.com>
2023-10-30 22:06:21 +0800

    配置项目pom文件，搭建项目框架

---
+++ mall-demo/src/test/java/com/answer/mall/demo/MallDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.answer.mall.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-security/src/test/java/com/answer/mall/security/MallSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.answer.mall.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
